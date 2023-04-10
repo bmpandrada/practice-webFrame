3f3b7b769684b9b540f55095d74f97ddbafbf8dc
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2023-04-10 22:28:46 +0800

    for web design only

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import img1 from '../assets/img/CACAO-DE-LILIO-logo-gold.png'
+import img2 from '../assets/img/CACAO-DE-LILIO-logo.png'
 
 const Featured = () => {
   return (
     <div className="container">
         <h2 className="heading-featured-in">As featured in</h2>
         <div className="logos">
-            <img src="/src/assets/img/CACAO-DE-LILIO-logo-gold.png" alt="" />
-            <img src="/src/assets/img/CACAO-DE-LILIO-logo.png" alt="" />
-            <img src="/src/assets/img/CACAO-DE-LILIO-logo-gold.png" alt="" />
-            <img src="/src/assets/img/CACAO-DE-LILIO-logo.png" alt="" />
-            <img src="/src/assets/img/CACAO-DE-LILIO-logo-gold.png" alt="" />
+            <img src={img1} alt="" />
+            <img src={img2} alt="" />
+            <img src={img1} alt="" />
+            <img src={img2} alt="" />
         </div>
     </div>
   )
